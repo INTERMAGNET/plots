d60a1255bb8f00fc275e8e345431798e1a8f4ee7
Charles Blais <charles.blais@canada.ca>
2019-07-18 17:37:11 +0000

    Updates

---
+++ precache-manifest.b69ffe85e43a44e411c83d1314af514c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6beea7b87615a47cb13dbebaa88d08ca",
+    "url": "/plots/index.html"
+  },
+  {
+    "revision": "ca29abb6844c50f3d30e",
+    "url": "/plots/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "5e0cd1ea124c780422f2",
+    "url": "/plots/static/js/2.2aae7e02.chunk.js"
+  },
+  {
+    "revision": "ca29abb6844c50f3d30e",
+    "url": "/plots/static/js/main.48298616.chunk.js"
+  },
+  {
+    "revision": "b4d2cdf70fc6f87a7627",
+    "url": "/plots/static/js/runtime~main.19e39859.js"
+  }
+]);
